3382a5eb849cd58f3f7f8096e50ad464eb053bbb
aniketrb-github <aniketbharsakale@gmail.com>
2019-02-03 20:22:29 +0530

    added Supplier FI to excell

--- javabrains_demo/src/main/java/com/javabrains/streams/StreamDemo1.java
+++ javabrains_demo/src/main/java/com/javabrains/streams/StreamDemo1.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.javabrains.exercise_one.Person;
 
 public class StreamDemo1 {
-
+	
 	public static void main(String[] args) {
 		// Initialize & create data
 		List<Person> peopleList = Arrays.asList(
